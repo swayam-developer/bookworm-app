be2ef1ca7754be115a8bbd5b0e2c9bacc7ce81f3
swayam-developer <swayamsathawane@gmail.com>
2026-01-29 15:18:19 +0530

    file upload storage increases

--- src/index.js
+++ src/index.js
@@ -15,8 +15,18 @@ connectDB();
 
 job.start();
 console.log("🕒 Cron job started — sending keep-alive every 14 minutes.");
-app.use(express.json());
-app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8081", // Expo web
+      "https://bookworm-app-wlck.onrender.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
